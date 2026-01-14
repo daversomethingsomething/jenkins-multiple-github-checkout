4ec5f21d841af37132bcaba0f58cea04db9aa67c
David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>
2026-01-14 16:17:14 +0000

    Initial version of MultipleGitHubCheckout.groovy, working groovydoc
    Signed-off-by: David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>

--- generateDocsRelease.sh
+++ generateDocsRelease.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Build out any groovydoc docstrings available
-groovydoc --destdir ./docs/groovydoc --sourcepath src/ org.DaverSomethingSomething.MultipleGitHubCheckout
+groovydoc -destdir ./docs/groovydoc \
+          -sourcepath src/ \
+          -verbose \
+          -nomainforscripts \
+          org.DaverSomethingSomething
 
 # Build the mkdocs site onto the `gh-pages` branch and version it
 mike deploy v1.0 latest
---
+++ src/org/DaverSomethingSomething/MultipleGitHubCheckout.groovy
@@ -0,0 +1,62 @@
+#!/usr/bin/env groovy
+
+/*
+ * Copyright (c) 2026, David L. Armstrong
+ *
+ * This package is distributed under the terms of the Apache License 2.0.
+ * For more details, see the full text of the license in the file LICENSE.txt.
+ *
+ */
+
+import java.nio.file.Path
+import java.nio.file.Paths
+
+/**
+ * Clones one or more Git repos public or private.
+ * Accepts a Map parameter containing name/value parameters as follows.
+ * 
+ * @param credentialsId ID for the credentials entry in Jenkins to use for authorization
+ * @param branch Branch to checkout (default: main)
+ * @param shallow Create shallow clones (no history)
+ * @param lfs Enable Git Large File Storage (LFS)
+ * @param repoUrls List of repos to clone
+ * @return `true` on success, throws exception otherwise.
+ *
+ */
+def multiClone(Map params){
+
+    branchCheckout = null
+    if(params.branch){
+        branchCheckout = params.branch
+    }
+    extensionsList = []
+    if(params.lfs) {
+        extensionsList.add(lfs())
+    }
+    if(params.shallow) {
+        extensionsList.add(cloneOption(shallow: true))
+    }
+
+    for( repoUrl in params.repoUrls ) {
+
+        subdirectory = "." // default for a single repo
+
+        // If more than one repo, grab the last part of the URL path
+        // for the subdirectory name.
+        if(params.repoUrls.size() > 1) {
+            urlObj = repoUrl.toURL()
+            Path filePath = Paths.get(urlObj.getPath())
+            subdirectory = filePath.getFileName()
+        }
+
+        dir(subdirectory) {
+            println "URL: ${repoUrl} Subdir: ${subdirectory}"
+            checkout scmGit(
+                branches: [[name: branchCheckout]],
+                extensions: extensionList,
+                userRemoteConfigs: [[credentialsId: params.credentialsId,
+                    url: repoUrl]]
+            )
+        }
+    }
+}
